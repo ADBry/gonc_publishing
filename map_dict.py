6de6b41383ec9ae243ca3a8a9470897b34fd79c3
ADBry <abryson@vhb.com>
2024-07-18 09:27:43 -0400

    Rewrote map dictionary to store map names instead of objects in memory

--- map_dict.py
+++ map_dict.py
@@ -2,14 +2,15 @@ import arcpy
 from pathlib import Path
 from pro_utils import MapLUT
 
-DEFAULT_OUTPUT = Path(r"S:\Mapping\MapProd\WebPublishing")
+# DEFAULT_OUTPUT = Path(r"S:\Mapping\MapProd\WebPublishing")
+DEFAULT_OUTPUT = Path(r"C:\Users\ext-adbryson\Documents\ArcGIS\Projects\Annotation\Scripted")
 
 class ProProject(arcpy.mp.ArcGISProject):
 
     def __init__(self, aprx_path, outpath:Path = DEFAULT_OUTPUT):
         super().__init__(aprx_path)
         self.maps = False
-        self.output_folder = outpath / "Output Files" / "ScriptTest3"
+        self.output_folder = outpath / "Output Files" / "Script Test"
         self.output_folder.mkdir(parents=True, exist_ok=True)
 
     def setup_maps(self):
@@ -22,9 +23,11 @@ class ProProject(arcpy.mp.ArcGISProject):
         m = self.maps
         for i in args:
             m = m[i]
+        tilefile = "".join(args)
+        m = self.listMaps(m)[0]
         lyr_for_AOI = m.listLayers("*County Boundary*")[0]
         # extent_for_AOI = arcpy.Describe(lyr_for_AOI).extent
-        tilename = (self.output_folder / "tiles.tpkx").as_posix()
+        tilename = (self.output_folder / f"{tilefile}.tpkx").as_posix()
 
         print("Processing tile cache...")
         # logMessage("Processing tile cache...")
@@ -33,7 +36,7 @@ class ProProject(arcpy.mp.ArcGISProject):
             service_type="ONLINE",
             output_file=tilename,
             format_type="PNG8",
-            level_of_detail=13,
+            level_of_detail=16,
             compression_quality=75,
             package_type="tpkx",
             min_level_of_detail=12,
